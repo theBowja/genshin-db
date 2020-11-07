7c1bd1f7a8af356e19737f549f77e02527481915
theBowja <ericxin@gmail.com>
2020-11-06 22:53:04 -0600

    cleaned up options

--- src/index.js
+++ src/index.js
@@ -1,37 +1,49 @@
 const genshin = {};
 
-let baselang = "english";
+let baseoptions = {
+    autocomplete: true,
+    resultlanguage: "english",
+    querylanguages: ["english"]
+}
 
 genshin.setOptions = function(options) {
-    options = formatOptions(options);
+    Object.assign(baseoptions, sanitizeOptions(options));
+}
 
-    if(options.language !== undefined) baselang = options.language;
+genshin.getOptions = function() {
+    return JSON.parse(JSON.stringify(baseoptions));
 }
 
 /**
- * remove this function later maybe
+ * Creates and returns a new object with 
  */
-genshin.setBaseLanguage = function(lang) {
-    lang = formatLanguages(lang);
-    if(lang === undefined) return false;
-    baselang = lang;
-    return true;
+function combineObjects(base, ...over) {
+
 }
 
 /**
- * 
+ * @param langs - a string or array of strings
+ * @returns undefined if none of the strings are valid languages
  */
-function formatLanguages(lang) {
-    switch(lang) {
-        case "en":
-        case "english":
-            return "english";
-        case "jp":
-        case "japanese":
-            return "japanese";
-        default:
-            return undefined;
+function formatLanguages(langs) {
+    if(typeof langs === "string") {
+        switch(langs) {
+            case "en":
+            case "english":
+                return "english";
+            case "jp":
+            case "japanese":
+                return "japanese";
+        }
+    } else if(Array.isArray(langs)) {
+        let tmp = [];
+        for(let l of langs) {
+            l = formatLanguages(l);
+            if(l && !tmp.includes(l)) tmp.push(l);
+        }
+        if(tmp.length) return tmp;
     }
+    return undefined;
 }
 
 function getJSON(path) {
@@ -45,48 +57,60 @@ function getJSON(path) {
 /**
  * get rid of unnecessary properties
  */
-function formatOptions(options={}) {
-    let opt = {};
-    opt.language = formatLanguages(options.language);
-    return opt;
+function sanitizeOptions(opts={}) {
+    opts.resultlanguage = formatLanguages(opts.resultlanguage);
+    opts.querylanguages = formatLanguages(opts.querylanguages);
+    let sanOpts = {};
+    if(opts.autocomplete === true || opts.autocomplete === false)
+        sanOpts.autocomplete = opts.autocomplete;
+    if(opts.resultlanguage)
+        sanOpts.resultlanguage = opts.resultlanguage;
+    if(opts.querylanguages)
+        sanOpts.querylanguages = opts.querylanguages;
+    return sanOpts;
 }
 
-genshin.characters = function(query, options={}) {
-    options = formatOptions(options);
+genshin.characters = function(query, opts={}) {
+    opts = Object.assign({}, baseoptions, sanitizeOptions(opts));
 
-    const index = getJSON('./index/characters.json');
-    const filename = index[baselang].file[index[baselang].name.indexOf(query)];
+    const queryindex = getJSON(`./index/${opts.resultlanguage}/characters.json`);
+    const filename = queryindex.file[queryindex.name.indexOf(query)];
     if(filename === undefined) return;
-    return getJSON(`./${baselang}/characters/${filename}`);
+    
+    return getJSON(`./${opts.resultlanguage}/characters/${filename}`);
 }
 
-genshin.elements = function(query, options={}) {
-    options = formatOptions(options);
+genshin.elements = function(query, opts={}) {
+    opts = Object.assign({}, baseoptions, sanitizeOptions(opts));
 
     const data = getJSON(`./${baselang}/elements/${query}`);
 
     return data;
 }
 
+genshin.categories = function(query, opts={}) {
+    opts = Object.assign({}, baseoptions, sanitizeOptions(opts));
 
-genshin.weapons = function(query, options) {
-    options = formatOptions(options);
+}
+
+genshin.weapons = function(query, opts={}) {
+    opts = Object.assign({}, baseoptions, sanitizeOptions(opts));
 
     const data = getJSON(`./${baselang}/weapons/${query}`);
 
     return data;
 }
 
-genshin.rarity = function(query, options) {
-    options = formatOptions(options);
+genshin.rarity = function(query, opts={}) {
+    opts = Object.assign({}, baseoptions, sanitizeOptions(opts));
 
     const data = getJSON(`./${baselang}/rarity/${query}`);
 
     return data;
 }
 
-genshin.reactions = function(query, options) {
-    options = formatOptions(options);
+genshin.reactions = function(query, opts={}) {
+    opts = Object.assign({}, baseoptions, sanitizeOptions(opts));
 
     const data = getJSON(`./${baselang}/reactions/${query}`);
 
