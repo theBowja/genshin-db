e5f9ecdd129b0eae340e8e6c903e90fa42f05cb6
theBowja <ericxin@gmail.com>
2021-09-09 18:30:14 -0400

    make Languages and Folder enum available
    #36

---
+++ src/folder.js
@@ -0,0 +1,19 @@
+// enum for folders. // make sure update index.d.ts too
+module.exports = {
+	characters     : 'characters',
+	talents        : 'talents',
+	constellations : 'constellations',
+	
+	weapons        : 'weapons',
+
+	foods          : 'foods',
+	materials      : 'materials',
+	weaponmaterialtypes: 'weaponmaterialtypes',
+	talentmaterialtypes: 'talentmaterialtypes',
+
+	artifacts      : 'artifacts',
+	domains        : 'domains',
+
+	rarity         : 'rarity',
+	elements       : 'elements'
+};
