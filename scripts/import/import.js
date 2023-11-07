dfb03c6956ce35cc743d25cc00d034a04a5b784e
theBowja <ericxin@gmail.com>
2023-11-06 23:56:26 -0500

    4.2 tcg

--- scripts/import/import.js
+++ scripts/import/import.js
@@ -533,36 +533,36 @@ gameVersion = "4.2"; // new data will use this as added version
 // importCurve('characters');
 // getUpperBodyImages(); // grabbing cover1, cover2 from official genshin impact site, // MUST IMPORT SEPARATELY FROM import characters
 
-importData('constellations');
-importData('talents');
-importData('weapons')
-importCurve('weapons');
-importData('artifacts');
-importData('foods');
-importData('materials');
-importData('domains');
-importData('enemies');
-importCurve('enemies');
+// importData('constellations');
+// importData('talents');
+// importData('weapons')
+// importCurve('weapons');
+// importData('artifacts');
+// importData('foods');
+// importData('materials');
+// importData('domains');
+// importData('enemies');
+// importCurve('enemies');
 
-importData('outfits');
-importData('windgliders');
-importData('animals');
-importData('namecards');
-importData('geographies');
-importData('crafts');
-importData('achievements');
-importData('achievementgroups');
-importData('adventureranks'); // max 60
+// importData('outfits');
+// importData('windgliders');
+// importData('animals');
+// importData('namecards');
+// importData('geographies');
+// importData('crafts');
+// importData('achievements');
+// importData('achievementgroups');
+// importData('adventureranks'); // max 60
 
-// importData('tcgcharactercards');
-// importData('tcgactioncards');
-// importData('tcgcardbacks');
-// importData('tcgcardboxes');
-// importData('tcgdetailedrules');
-// importData('tcgkeywords');
-// importData('tcglevelrewards');
-// importData('tcgstatuseffects');
-// importData('tcgsummons');
+importData('tcgcharactercards');
+importData('tcgactioncards');
+importData('tcgcardbacks');
+importData('tcgcardboxes');
+importData('tcgdetailedrules');
+importData('tcgkeywords');
+importData('tcglevelrewards');
+importData('tcgstatuseffects');
+importData('tcgsummons');
 
 // updateURLs(); // must be separate
 
