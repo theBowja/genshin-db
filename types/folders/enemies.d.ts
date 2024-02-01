4c8e46f315ff9189dcd753ac373eff7a21cb2c77
theBowja <ericxin@gmail.com>
2024-02-01 02:41:34 -0500

    fix enemies
    breaking changes with enemies data. check the types diff for enemies.d.ts
    
    #165

--- types/folders/enemies.d.ts
+++ types/folders/enemies.d.ts
@@ -1,16 +1,22 @@
 declare module "genshin-db" {
 	export interface Enemy {
 		id: number;
-		name: string;
-		specialName: string;
+		monsterId: number;
 
+		name: string;
+		specialNames: string[];
+
+		monsterType: 'MONSTER_BOSS' | 'MONSTER_ORDINARY'; // enum
 		enemyType: 'BOSS' | 'COMMON' | 'ELITE'; // enum
-		category: string; // translated
+		categoryType: 'CODEX_SUBTYPE_ABYSS' | 'CODEX_SUBTYPE_AUTOMATRON' | 'CODEX_SUBTYPE_BEAST' | 'CODEX_SUBTYPE_BOSS' | 'CODEX_SUBTYPE_ELEMENTAL' | 'CODEX_SUBTYPE_FATUI' | 'CODEX_SUBTYPE_HILICHURL' | 'CODEX_SUBTYPE_HUMAN'; // enum
+		categoryText: string; // translated
 		description: string;
 
 		investigation?: { // almost all but not every enemy has this.
+			investigationId: number;
 			name: string;
-			category: string;
+			categoryType: 'Boss' | 'Common' | 'Elite'; // enum
+			categoryText: string; // translated
 			description: string;
 		}
 
@@ -30,7 +36,7 @@ declare module "genshin-db" {
 
 		images: {
 			filename_icon: string;
-			filename_investigationIcon: string;
+			filename_investigationIcon?: string;
 		};
 		stats: EnemyStatFunction;
 	}
